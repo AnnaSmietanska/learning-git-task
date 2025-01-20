a952944a3bb73124fa01b3baa347ce21721d1811
Anna Śmietańska <anna.smietanska90@gmail.com>
2025-01-20 15:07:11 +0100

    Init file

---
+++ main.py
@@ -0,0 +1,13 @@
+Lista_zakupow = {
+    "piekarnia": ['chleb', 'pączek', 'bułki'], 
+    "warzywniak": ['marchew', 'seler', 'rukola']
+}
+
+print("Lista zakupów")
+for key, values in Lista_zakupow.items():
+    key_capitalize = key.capitalize()
+    value_capitalize = [item.capitalize() for item in values]
+    print(f"Idę do {key_capitalize}, kupuję tu następujące rzeczy: {value_capitalize} ")
+
+value_number = sum(len(value) for value in Lista_zakupow.values())
+print(f"W sumie kupuję {value_number} produktów")
